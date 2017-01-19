c7c0101041cd26cbd27db043bad21ec2521f7d40
Casey Webb <notcaseywebb@gmail.com>
2017-01-19 10:28:28 -0600

    Add overlay (for readability)

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
+'use strict'
+
 const { nasaApiKey } = require('./nasa-api-key');
 
 exports.decorateConfig = (config) => {
+  const { overlayColor = '#000', overlayOpacity = .5 } = config.hypernasa || {};
+
   return Object.assign({}, config, {
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
+    css: `
+      ${config.css}
+
+      .hyper_main {
+        background-color: ${overlayColor};
+        background-size: cover;
+        background-position: center;
+      }
+
+      .hyper_main:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: ${overlayColor};
+        opacity: ${overlayOpacity};
+      }
+    `
   });
 }
 
-exports.decorateHyper = (Hyper, { React }) => {
-
-  return class extends React.Component {
+exports.decorateHyper = (Hyper, { React }) =>
+  class extends React.Component {
     constructor (props, context) {
       super(props, context);
-      this._fetchImage = this._fetchImage.bind(this);
       this._fetchImage();
     }
 
     _fetchImage () {
-      const potdImageUrl= `https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`;
-
-      fetch(potdImageUrl).then((response) => {
-        response.json().then((data) => {
-          this.setState({image: data.hdurl || data.url});
-        });
-      });
+      fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`)
+        .then((response) => response.json())
+        .then(({ hdurl, url }) => this.setState({ image: hdurl || url }));
     }
 
     render () {
-      let css;
-
-      if (this.state && this.state.image) {
-        css = `
-          .hyper_main {
-            background-image: url(${this.state.image});
-            background-size: cover;
-            background-color: #000;
-          }`;
-      } else {
-        css = `
-          .hyper_main {
-            background-color: #000;
-          }`;
-      }
-
       return React.createElement(Hyper, Object.assign({}, this.props, {
-        customCSS: css
+        customCSS: `
+          ${this.props.customCSS}
+
+          .hyper_main {
+            background-image: url("${this.state && this.state.image}");
+          }
+        `
       }));
     }
-  }
-};
+  };
